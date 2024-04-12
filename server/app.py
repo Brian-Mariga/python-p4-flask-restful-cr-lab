41ad7fa3cbacdecf6012fdb09ab24ef0879d7397
Brian <brianmariga0@gmail.com>
2024-04-12 08:18:05 +0300

    Copleted assignment

--- server/app.py
+++ server/app.py
@@ -17,11 +17,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+
+        return make_response(plants, 200)
+    
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(name=data['name'], image=data['image'], price=data['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+
+        plant_dict = new_plant.to_dict()
+
+        return make_response(plant_dict, 201)
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        if plant := Plant.query.filter(Plant.id == id).first():
+            plant_dict = plant.to_dict()
+            return make_response(plant_dict, 200)
+        else:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
